2f02c43717614942d19ff8cf6bf793ce504abb31
Anuj Sachdeva <anujsachdeva100@gmail.com>
2018-04-17 12:35:25 +0530

    added the preview option. not working quite the way it should

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
+	//the main article window
 	const $pageContainer = $('#main-article');
 
+
+	function prepareForClipping(){
+	  isClipping = true;
+	  if(clippingObj) {
+	    clippingObj.setOptions({ show: true, persistent: true, enable:true });
+	  }
+	}
+
 	function clipSelectionChange(e){
+		$('#clip-options').remove();
 	  var pageOffset = $pageContainer.offset();
-	  console.log('pageOffset = ' + pageOffset.top)
 	  var clipSelection = clippingObj.getSelection();
-	  var firstTime = false;
+		var firstTime = (e ? e.firstTime : false);
+
 	  if(clipSelection.width < 10){
 	    return;
 	  }
@@ -16,14 +26,68 @@ $(document).ready(function () {
 	    (firstTime ? '<p>Re-size and place this box where you want to clip</p>' : '')+
 	    (firstTime ? '' : '<button id="clip-options-save" class="btn btn-primary">Clip &amp; Share</button>') +
 	    '<button id="clip-options-cancel" class="btn btn-link"><i class="fa fa-times"></i> Cancel</button></div></div>');
-}
+	  $('#clip-options-cancel').click(cancelClipping);
+	  $('#clip-options-save').click(saveClipping);
+	  preview(document.getElementById('photo'), clipSelection);
+	  
+	}
+
+	function saveClipping() {
+		$('#clip-options').remove();
+	  var selection = clippingObj.getSelection();
+	  if((selection.width < 50) || (selection.height) < 50){
+	    alert('Very small clip. Please increase the size');
+	    return;
+	  }
+	}
+
+	$('<div><img src="img/page.png" style="position: relative;" /><div>')
+    .css({
+        float: 'left',
+        position: 'relative',
+        overflow: 'hidden',
+        top: '-100%',
+        left: '-30%',
+        width: '100px',
+        height: '100px'
+    })
+    .insertAfter($('#photo'));
+
+	function cancelClipping(){
+	  if(clippingObj) {
+	    clippingObj.cancelSelection();  
+	  } 
+	  $('#clip-options').remove();
+	  isClipping = false;
+	  if(clippingObj) {
+	    clippingObj.setOptions({ persistent: false });
+	    clippingObj.update();
+	  }
+//	  $('#page-level-bar').removeClass('flip');
+	}
 
   const clippingObj = $('img#photo').imgAreaSelect({
-    handles: true,
-    onSelectEnd: console.log('selected'),
-    keys: true,
-    instance: true,
-    onSelectEnd: clipSelectionChange,
+    'handles': true,
+    'parent':'body',
+    'instance':true,
+    'fadeSpeed':300,
+    'keys': true,
+    'onSelectStart': prepareForClipping,
+    'onSelectChange': clipSelectionChange,
+    'aspectRatio': '1:1'
   });
+
+  function preview(img, selection) {
+	  var scaleX = 100 / (selection.width || 1);
+	  var scaleY = 100 / (selection.height || 1);
+
+	  $('#photo + div > img').css({
+	    width: Math.round(scaleX * 400) + 'px',
+	    height: Math.round(scaleY * 300) + 'px',
+	    marginLeft: '-' + Math.round(scaleX * selection.x1) + 'px',
+	    marginTop: '-' + Math.round(scaleY * selection.y1) + 'px'
+	  });
+	}
+
 });
 
